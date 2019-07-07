1aa9859aaf451b794f03bdd21d23323b2ea7f420
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-07-08 00:29:30 +0530

    Add food and snake size logic

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,34 @@
-var snake;
+let snake;
+let size = 10;
+let food;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(600, 600);
   snake = new Snake();
+  frameRate(10);
+  generateFood();
+}
+
+// Make food appear in correct grid
+function generateFood() {
+  let cols = floor(width/size);
+  let rows = floor(height/size);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(size);
 }
 
 function draw() {
   background(50);
   snake.update();
   snake.show();
+
+  if(snake.eat(food)){
+    snake.total++;
+    generateFood();
+  }
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, size, size);
 }
 
 function keyPressed() {
--- js/snake.js
+++ js/snake.js
@@ -3,20 +3,40 @@ function Snake() {
   this.y = 0;
   this.xspeed = 1;
   this.yspeed = 0;
+  this.total = 0; // size of tail
+  this.tail = []; // stores snake's body except head
 
   this.show = function() {
     fill(255);
     noStroke();
-    rect(this.x, this.y, 10, 10);
+    for(let i=0; i<this.tail.length; i++){
+      rect(this.tail[i].x, this.tail[i].y, size, size);
+    }
+    rect(this.x, this.y, size, size);
   }
 
   this.update = function() {
-    this.x = this.x + this.xspeed;
-    this.y = this.y + this.yspeed;
+    if(this.total === this.tail.length){
+      for(let i=0; i<this.tail.length-1; i++){
+        this.tail[i] = this.tail[i+1];
+      }
+    }
+    this.tail[this.total-1] = createVector(this.x, this.y);
+
+    this.x = this.x + this.xspeed*size;
+    this.y = this.y + this.yspeed*size;
+    this.x = constrain(this.x, 0, width-size);
+    this.y = constrain(this.y, 0, height-size);
   }
 
   this.dir = function(x, y) {
     this.xspeed = x;
     this.yspeed = y;
   }
+
+  this.eat = function(food){
+    let d = dist(this.x, this.y, food.x, food.y);
+    return (d < 1);
+  }
+
 }
